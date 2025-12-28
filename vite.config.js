9410fcda913047fd57afd89b1a71d9e8d9d76bd1
sushmitagiri <sushmita.g@ampityinfotech.com>
2025-12-28 16:30:14 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Community-Animal-Welfare-Support-Portal/',
   server: {
     port: 3000,
     open: true
